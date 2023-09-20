184f38aa02aaae04087c905009801c0728fe9fa4
tnswo6720 <ghd67300@gmail.com>
2023-09-21 00:46:50 +0900

    projectone

---
+++ src/components/commons/layout/header/LayoutHeader.presenter.tsx
@@ -0,0 +1,21 @@
+import {
+  InnerButton,
+  InnerLogo,
+  InnerWrapper,
+  Wrapper,
+} from "./LayoutHeader.styles";
+import type { ILayoutHeaderProps } from "./LayoutHeader.types";
+
+export default function LayoutHeaderUI(props: ILayoutHeaderProps): JSX.Element {
+  return (
+    <Wrapper>
+      <InnerWrapper>
+        <InnerLogo onClick={props.onClickLogo}>💎 Solo</InnerLogo>
+        <div>
+          <InnerButton onClick={props.onClickMoveToLogin}>로그인</InnerButton>
+          <InnerButton>회원가입</InnerButton>
+        </div>
+      </InnerWrapper>
+    </Wrapper>
+  );
+}
